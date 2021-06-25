cae4b66cb15d1dd16220aa647f2fd7dbb35d69c1
Luka Maisuradze <lmaisuradz3@gmail.com>
2021-06-25 16:33:16 +0400

    Fix wrong file names

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -1,12 +1,16 @@
 import { Subjects } from "./subjects";
 import { OrderStatus } from "./types/order-status";
 
-export interface OrderCancelledEvent {
-  subject: Subjects.OrderCancelled;
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
   data: {
     id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
     ticket: {
       id: string;
+      price: number;
     };
   };
 }
